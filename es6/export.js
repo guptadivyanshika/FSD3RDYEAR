03912c7d023c6fc8bad2701c937c3a1c7feb0483
divyanshika <guptadivyanshika16@gmail.com>
2025-10-14 11:50:51 +0530

    y

---
+++ es6/export.js
@@ -0,0 +1,7 @@
+export const add=(a, b)=>a+b;
+export const sub=(a, b)=>a-b;
+
+export default function divide(a,b){
+  if(b==0)return;
+  return a/b;
+}
---
+++ es6/import.js
@@ -0,0 +1,4 @@
+import divide,{add,sub} from './export.js';
+console.log(add(2,3));
+console.log(sub(6,3));
+console.log(divide(4,2));
